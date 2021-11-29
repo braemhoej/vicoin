76ab3f14f1cffec97a6a3f4f21e5394825806407
Nicolaj Kuno Bræmhøj <nicolaj@braemhoej.me>
2021-11-29 22:04:18 +0000

    Improve type safety, return error no panic

--- crypto/key.go
+++ crypto/key.go
@@ -1,6 +1,7 @@
 package crypto
 
 import (
+	"errors"
 	"math/big"
 	"vicoin/encoding"
 )
@@ -14,12 +15,16 @@ func (privateKey *PrivateKey) ToString() (string, error) {
 	return encoding.ToB64(privateKey)
 }
 func (privateKey *PrivateKey) FromString(str string) (*PrivateKey, error) {
-	key, err := encoding.FromB64(str)
+	encodedKey, err := encoding.FromB64(str)
 	if err != nil {
 		return nil, err
 	}
-	privateKey.D = key.(PrivateKey).D
-	privateKey.N = key.(PrivateKey).N
+	switch key := encodedKey.(type) {
+	case PrivateKey:
+		privateKey = &key
+	default:
+		return nil, errors.New("decoded string is not a private key")
+	}
 	return privateKey, nil
 }
 
@@ -32,11 +37,15 @@ func (publicKey *PublicKey) ToString() (string, error) {
 	return encoding.ToB64(publicKey)
 }
 func (publicKey *PublicKey) FromString(str string) (*PublicKey, error) {
-	key, err := encoding.FromB64(str)
+	encodedKey, err := encoding.FromB64(str)
 	if err != nil {
 		return nil, err
 	}
-	publicKey.E = key.(PublicKey).E
-	publicKey.N = key.(PublicKey).N
+	switch key := encodedKey.(type) {
+	case PublicKey:
+		publicKey = &key
+	default:
+		return nil, errors.New("decoded string is not a public key")
+	}
 	return publicKey, nil
 }
